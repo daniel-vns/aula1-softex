4d47361991054ab76d610634e54f6d4125906033
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-07-18 18:52:08 +0000

    aula1

---
+++ aula1.js
@@ -0,0 +1,9 @@
+console.log("======== MENU ========");
+console.log("1 - Listar Clientes");
+console.log("2 - Cadastrar Clientes");
+console.log("3 - Editar Clientes");
+console.log("4 - Remover Clientes");
+console.log("0 - Sair do Sistema");
+console.log("======================");
+console.log("Opção: __");
+console.log("======================")
