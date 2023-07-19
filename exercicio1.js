aef287867e08ba127acb173cfba71063c1f3800c
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-07-19 20:00:15 +0000

    exercicio1

--- exercicio1.js
+++ exercicio1.js
@@ -4,7 +4,7 @@ qntd_rodas = 8;
 peso_bruto = 10000;
 qntd_pessoas = 30;
 
-if (qntd_rodas == 2 && 3) { 
+if (qntd_rodas == 2 || qntd_rodas == 3) { 
     console.log("Habilitação A")
 } else if (qntd_rodas == 4 && qntd_pessoas <=8 && peso_bruto <=3500) {
         console.log("Habilitação B")
