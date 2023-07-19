6b5c5f69d369cd3a42d612264bbbb7afff40cd1b
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-07-19 19:54:21 +0000

    exercicio1

---
+++ exercicio1.js
@@ -0,0 +1,20 @@
+var qntd_rodas, peso_bruto, qntd_pessoas;
+
+qntd_rodas = 8;
+peso_bruto = 10000;
+qntd_pessoas = 30;
+
+if (qntd_rodas == 2 && 3) { 
+    console.log("Habilitação A")
+} else if (qntd_rodas == 4 && qntd_pessoas <=8 && peso_bruto <=3500) {
+        console.log("Habilitação B")
+        } else if (qntd_rodas >= 4 && peso_bruto >= 3500 && peso_bruto<= 6000) {
+            console.log("Habilitação c")
+                } else if (qntd_rodas >= 4 && qntd_pessoas >= 8) {
+                    console.log("Habiitação D")
+                       } else if (qntd_rodas >= 4 && peso_bruto > 6000){
+                           console.log("Habilitação E")
+                             }
+
+                
+
