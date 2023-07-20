14a8a1ccd3f277b30c53e3b383b755317e56e263
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-07-20 17:44:39 +0000

    aula2

---
+++ aula2.js
@@ -0,0 +1,27 @@
+var minhaVar;
+minhaVar = 10;
+var soma = 15 + 8;
+soma += 1;
+soma *= 3;
+console.log(soma);
+
+var fazSol = true;
+var comida = ["arroz", "feijao", "ovo"];
+console.log(comida[1]);
+
+if (soma === minhaVar){
+    console.log("verdade")
+    }else{ 
+        console.log("falso")
+}
+
+if (minhaVar <= soma){
+    console.log("verdade")
+    }else{ 
+        console.log("falso")
+}
+
+function divisao (a,b){
+    return a/b;
+}
+console.log(divisao(10,2));
