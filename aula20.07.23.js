fd881c3c4d578011147d8d980562d4e5fe2ab3f4
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-07-20 19:05:40 +0000

    aula20.07.23

---
+++ aula20.07.23.js
@@ -0,0 +1,34 @@
+let num = 0;
+let nome = "daniel";
+let valor = true;
+if(num>0){
+    console.log("numero é positivo")
+}else if(num<0){
+    console.log("numero é negativo")
+}else{
+    console.log("numero é neutro")
+}
+
+if(num%2===0){
+    console.log("numero é par")
+}else{
+    console.log("numero é negativo")
+}
+
+if(nome = null){
+    console.log("string vazia")
+}else{
+    console.log("string não é vazia")
+}
+
+if(num>10){
+    console.log("verdade")
+}else{
+    console.log("falso")
+}
+
+if(valor = Boolean){
+    console.log("verdade")
+}else{
+    console.log("falso")
+}
