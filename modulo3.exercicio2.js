5b64fff22829b6e8c799bfe1ab864126eab73b82
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-08-07 17:53:36 +0000

    modulo3.exercicio2

---
+++ modulo3.exercicio2.js
@@ -0,0 +1,8 @@
+let matriz = new Array(4);
+
+for(let i=0; i<4; i++){
+    for(let j=0; j<4[i]; j++){
+        console.log("animal1", "animal2", "animal3", "animal4");
+    }    
+    console.log("cachorro", 3, "anos", "papagaio", 4, "anos")       
+}
