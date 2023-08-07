133d25e05fcda0d7d9166513a0e8eee1c8581cb0
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-08-07 18:03:21 +0000

    modulo3.exercicio2

--- modulo3.exercicio2.js
+++ modulo3.exercicio2.js
@@ -1,8 +1,11 @@
-let matriz = new Array(4);
+let animal = [
+    ["gato", "felino", "mia", 3],
+    ["cachorro", "mamifero", "floque", 10],
+    ["galinha", "ave", "cristal", 5]
+];
 
-for(let i=0; i<4; i++){
-    for(let j=0; j<4[i]; j++){
-        console.log("animal1", "animal2", "animal3", "animal4");
-    }    
-    console.log("cachorro", 3, "anos", "papagaio", 4, "anos")       
+for (let i = 0; i < animal.length; i++) {
+    for (let j = 0; j < animal[i].length; j++) {
+      console.log(animal[i][j]);
+    }
 }
