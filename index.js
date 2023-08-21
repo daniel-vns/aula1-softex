b97e106282d201ddf99e196dde371902a06e5977
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-08-21 19:56:38 +0000

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+const cadastro = {
+    nome: "daniel",
+    salario: 19000,
+    idade: 38,
+    diploma: true
+};
+
+console.log(typeof(cadastro.nome));
+console.log(typeof(cadastro.idade));
+console.log(typeof(cadastro.salario));
+console.log(typeof(cadastro.diploma));
