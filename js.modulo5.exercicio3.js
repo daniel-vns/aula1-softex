88bdfaf96dbe5ffb6e5ec1befbcdf221edc05574
Daniel Viana <108027997+daniel-vns@users.noreply.github.com>
2023-09-12 16:58:49 +0000

    js.modulo5.exercicio3

---
+++ js.modulo5.exercicio3.js
@@ -0,0 +1,7 @@
+class MinhaClasse {
+    static metodoEstatico() {
+      console.log('Este é um método estático da classe');
+    }
+  }
+  // Chamar o método estático diretamente na classe
+  MinhaClasse.metodoEstatico();
